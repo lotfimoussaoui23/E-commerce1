2bad48f04b7cdc35e5ef7f8d349c64f20d4ad0bb
lotfimoussaoui23 <lotfimoussaoui@gmail.com>
2026-08-23 10:36:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.static(__dirname));
 // ==========================
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "ecommerce",
-    ssl: process.env.DB_SSL === "true"
-        ? { rejectUnauthorized: false }
-        : undefined
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 // ==========================
